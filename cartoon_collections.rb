9856b244108801452ea6eb104e749ce2115a36dd
Tammy Metz <tammy@tammymetz.com>
2018-07-25 19:36:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each_with_index do |name, index|
-    puts "#{index+!}. #{name}"
+    puts "#{index+!}\. #{name}"
   end
 end
 
